e7f503f4e0c0402d680022945451e91498784145
Javohir <104622975+Javohirbek009@users.noreply.github.com>
2022-05-09 11:32:30 +0300

    Add files via upload

---
+++ port3 python/py.py
@@ -0,0 +1,21 @@
+########
+
+
+import time
+
+
+def poyga():
+    print("tayyorlaning...")
+    time.sleep(1)
+    print("3")
+    print("🔴")
+    time.sleep(1)
+    print("2")
+    print("🟡")
+    time.sleep(1)
+    print("1")
+    print("🟢")
+    time.sleep(1)
+    print("START")
+
+poyga()
